434360ed9a905a3973aeb9bd75902600bda0c06b
“rdpulgar” <66042231+rdpulgar@users.noreply.github.com>
2021-09-18 23:36:13 -0500

    update

--- spanish_sa_bert.py
+++ spanish_sa_bert.py
@@ -51,7 +51,7 @@ else:
 uploaded_file = st.file_uploader("O bien puede seleccionar un archivo CSV para procesar múltiples párrafos (se procesará columna 'text')",type=['csv'])
 if uploaded_file is not None:
     if st.button("Procesar Archivo CSV"):
-        data = pd.read_csv(uploaded_file,nrows=100)
+        data = pd.read_csv(uploaded_file,nrows=2000)
         st.success("Procesando CSV ..")
         data[data['text'].str.strip().astype(bool)]
         #indexes = data[data.text_len <  30].index
